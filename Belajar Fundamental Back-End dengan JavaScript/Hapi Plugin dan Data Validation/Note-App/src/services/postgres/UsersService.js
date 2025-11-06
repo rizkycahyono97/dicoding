40e49102dc29fe5b6fe9287309f28d654ea1ec3c
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-11-06 21:11:21 +0700

    add service for note-users

---
+++ Belajar Fundamental Back-End dengan JavaScript/Hapi Plugin dan Data Validation/Note-App/src/services/postgres/UsersService.js
@@ -0,0 +1,61 @@
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3) RETURNING id',
+      values: [id, username, password, fullname]
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User Gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username]
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError(
+        'Gagal menambahkan user. Username sudah digunakan'
+      );
+    }
+  }
+
+  async getUserById(userId) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+      values: [userId]
+    };
+
+    const result = this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('User tidak ditemukan');
+    }
+
+    return result.rows[0];
+  }
+}
+
+module.exports = UsersService;
