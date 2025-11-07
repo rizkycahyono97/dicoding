4ed1543ab39466b6539a2d0c4fea091fc3515107
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-11-07 16:49:00 +0700

    add user service and fix bug

--- Belajar Fundamental Back-End dengan JavaScript/Hapi Plugin dan Data Validation/Note-App/src/services/postgres/UsersService.js
+++ Belajar Fundamental Back-End dengan JavaScript/Hapi Plugin dan Data Validation/Note-App/src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -55,6 +56,28 @@ class UsersService {
     }
 
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, username, password FROM users WHERE username = $1',
+      values: [username]
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang anda berikan salah');
+    }
+
+    return id;
   }
 }
 
