ad3df869e6c6a426593335ec2ddb63e93910dc27
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-11-08 11:05:29 +0700

    add authentication api

--- Belajar Fundamental Back-End dengan JavaScript/Hapi Plugin dan Data Validation/Note-App/src/server.js
+++ Belajar Fundamental Back-End dengan JavaScript/Hapi Plugin dan Data Validation/Note-App/src/server.js
@@ -1,18 +1,28 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const ClientError = require('./exceptions/ClientError');
+
+// notes
 const notes = require('./api/notes');
-// const NotesService = require('./services/inMemory/NotesService');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
-const ClientError = require('./exceptions/ClientError');
+
+// users
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const notesService = new NotesService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -36,6 +46,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator
+      }
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator
       }
     }
   ]);
