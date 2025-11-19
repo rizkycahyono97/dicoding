375cb2ae8de73fd381c0d089d93c00e8571b7a8c
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2025-11-19 10:35:30 +0700

    add foreign key to notes owner column

--- Belajar Fundamental Back-End dengan JavaScript/Hapi Plugin dan Data Validation/Note-App/migrations/1763523175058_add-foreign-key-to-owner-column.js
+++ Belajar Fundamental Back-End dengan JavaScript/Hapi Plugin dan Data Validation/Note-App/migrations/1763523175058_add-foreign-key-to-owner-column.js
@@ -8,11 +8,17 @@ export const shorthands = undefined;
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const up = (pgm) => {};
+export const up = pgm => {
+  pgm.sql(
+    "INSERT INTO users(id, username, password, fullname) VALUES ('old_notes', 'old_notes', 'old_notes', 'old notes')"
+  );
+
+  pgm.sql("UPDATE notes SET owner = 'old_notes' WHERE owner IS NULL");
+};
 
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
  * @returns {Promise<void> | void}
  */
-export const down = (pgm) => {};
+export const down = pgm => {};
